42076e037eb02028eaddbc9379fc8aa4c270286e
jesimichele <jesicamichelle_lara@hotmail.com>
2024-03-26 16:47:58 -0300

    second commit

--- src/app.js
+++ src/app.js
@@ -9,28 +9,41 @@ const app = express()
 
 
 //Ruta products con query limit
-app.get("/products", (req, res) => {
+app.get("/products", async (req, res) => {
 
-    const { limit } = req.query;
+   try{
+     const { limit } = req.query;
 
     const prod = new ProductManager();
+    const productos= await prod.getProduct(limit)
 
-    return res.json({ productos: prod.getProduct(limit) })
+    return res.json({ productos })
+    }catch (error) {
+        console.error("Error al obtener productos:", error);
+        return res.status(500).json({ mensaje: "Error interno del servidor" });
+    }
 
 })
 
 
 //Ruta products con id especifico
-app.get("/products/:pid", (req, res) => {
-    const { pid } = req.params;
-    const prod = new ProductManager();
-    const producto = prod.getProductById(pid);
+app.get("/products/:pid", async (req, res) => {
 
-    if (producto !== "Not Found") {
-        return res.json({ producto });
-    } else {
-        return res.status(404).json({ mensaje: "Producto no encontrado" });
+    try {
+        const { pid } = req.params;
+        const prod = new ProductManager();
+        const producto = await prod.getProductById(pid);
+        
+        if (producto !== "Not Found") {
+            return res.json({ producto });
+        } else {
+            return res.status(404).json({ mensaje: "Producto no encontrado" });
+        }
+    } catch (error) {
+        console.error("Error al obtener producto por ID:", error);
+        return res.status(500).json({ mensaje: "Error interno del servidor" });
     }
+
 });
 
 
